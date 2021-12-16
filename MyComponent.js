134011879c645fb78ecf98990e0d82f482385e03
ino333 <liopku89@gmail.com>
2021-12-16 13:23:04 +0900

    ¸޼¼Áö

---
+++ MyComponent.js
@@ -0,0 +1,24 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MyComponent extends Component{
+    static defaultProps = {
+        name: '기본 이름'
+    };
+    static propTypes = { 
+        name: PropTypes.string,
+        favoriteNumber: PropTypes.number.isRequired
+    };
+    render(){
+        const {name, favoriteNumber, children} = this.props;    //비구조화 할당
+        return(
+        <div>
+            안녕, 내 이름은 {name}입니다. <br />
+            children 값은 {children} 입니다. <br />
+            내가 좋아하는 숫자는 {favoriteNumber}입니다.
+        </div>
+        );
+    };
+};
+
+export default MyComponent;
